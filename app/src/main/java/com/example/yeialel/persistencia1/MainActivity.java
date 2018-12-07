e282ca1113f01e5fe46ac7eb75eb0c427898df59
Tomas Enrique <tomasenriquea80@gmail.com>
2018-12-07 14:05:30 +0100

    Terminado
    Se agrego codigo para pantener el check.

--- app/src/main/java/com/example/yeialel/persistencia1/MainActivity.java
+++ app/src/main/java/com/example/yeialel/persistencia1/MainActivity.java
@@ -1,23 +1,18 @@
 package com.example.yeialel.persistencia1;
 
-import android.app.assist.AssistStructure;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText apodo, email, nombre, apellido;
     private String datoApodo, datoEmail,datoNombre, datoApellido;
 
-    private CheckBox estoyDeacuerdo;
-    private boolean datoEstoyDeacuerdo;
-
+    private CheckBox estado;
+    private boolean datoCheckBox;
 
     public SharedPreferences datos;
 
@@ -30,23 +25,18 @@ public class MainActivity extends AppCompatActivity {
         email = (EditText) findViewById(R.id.editTextEmail);
         nombre = (EditText) findViewById(R.id.editTextNombre);
         apellido = (EditText) findViewById(R.id.editTextApellido);
-        estoyDeacuerdo = (CheckBox) findViewById(R.id.checkBoxEstoyDeacuerdo);
-
+        estado = (CheckBox) findViewById(R.id.checkBoxEstoyDeacuerdo);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        //Toast toast = Toast.makeText(this, "Estoy en onPause", Toast.LENGTH_LONG);
-        //toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 1000);
-        //toast.show();
-
-        //PASOS PARA LA PERSISTENCIA DE DATOS
+        //PASOS PARA LA PERSISTENCIA DE DATOS PARA CARGAR LAS PREFERENCIAS
         /** 1 - Crear un objeto de tipo SharedPreferences, Con esto se crea un objeto donde se guardara los datos de la pantalla
          * El parametro: 'archivoConfiguracion' es el nombre de archivo donde se guardara las preferencias   */
         datos = getSharedPreferences("archivoPreferencias", MODE_PRIVATE);
 
-        // 2 - Hacer esitable el objeto creado, con esto se hace que el objeto SharedPreferences sea editable
+        // 2 - Hacer editable el objeto creado, con esto se hace que el objeto SharedPreferences sea editable
         SharedPreferences.Editor miEditor = datos.edit();
 
         // 3 - Establecer la informacion a almacenar,  Aqui se modifica el objeto para que almacene los datos que queremos
@@ -54,35 +44,32 @@ public class MainActivity extends AppCompatActivity {
         miEditor.putString("email", email.getText().toString());
         miEditor.putString("nombre", nombre.getText().toString());
         miEditor.putString("apellido", apellido.getText().toString());
-
+        miEditor.putBoolean("estado", estado.isChecked());  // verifica el estado en que esta el check
 
         // 4 - Transferir la informacion a SharedPreferences, Con esto hacemos que las preferencias sean compartidas
         miEditor.apply();  // Esto se lleva la informacion al objeto
-
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        //Toast toast = Toast.makeText(this, "Estoy en onResume", Toast.LENGTH_LONG);
-        //toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 600);
-        //toast.show();
 
         // Con esto se crea un objeto donde se guardo los datos de la pantalla
         datos = getSharedPreferences("archivoPreferencias", MODE_PRIVATE);
 
-        datoApodo = datos.getString("apodo", "nada");
+        datoApodo = datos.getString("apodo", "");
         apodo.setText(datoApodo);
 
-        datoEmail = datos.getString("email", "nada");
+        datoEmail = datos.getString("email", "");
         email.setText(datoEmail);
 
-        datoNombre = datos.getString("nombre", "nada");
+        datoNombre = datos.getString("nombre", "");
         nombre.setText(datoNombre);
 
-        datoApellido = datos.getString("apellido", "nada");
+        datoApellido = datos.getString("apellido", "");
         apellido.setText(datoApellido);
 
-
+        datoCheckBox = datos.getBoolean("checked", true);
+        estado.setChecked(datoCheckBox);
     }
 }
