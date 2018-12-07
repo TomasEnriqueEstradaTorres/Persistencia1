f924f68f9bc4cce927e1293e615a516769535662
Tomas Enrique <tomasenriquea80@gmail.com>
2018-12-07 09:03:17 +0100

    Terminadad persistencia SharedPreferences
    Se mantiene los datos, incluso si la aplicacion se cierra.
    Falta ver como agregar el checkBox

--- app/src/main/java/com/example/yeialel/persistencia1/MainActivity.java
+++ app/src/main/java/com/example/yeialel/persistencia1/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.yeialel.persistencia1;
 
+import android.app.assist.AssistStructure;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText apodo, email, nombre, apellido;
     private String datoApodo, datoEmail,datoNombre, datoApellido;
 
+    private CheckBox estoyDeacuerdo;
+    private boolean datoEstoyDeacuerdo;
+
+
     public SharedPreferences datos;
 
     @Override
@@ -21,24 +26,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        Toast toast = Toast.makeText(this, "Estoy en onResume", Toast.LENGTH_LONG);
-        toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 600);
-        toast.show();
-        */
-
-
         apodo = (EditText) findViewById(R.id.editTextUserName);
         email = (EditText) findViewById(R.id.editTextEmail);
         nombre = (EditText) findViewById(R.id.editTextNombre);
         apellido = (EditText) findViewById(R.id.editTextApellido);
+        estoyDeacuerdo = (CheckBox) findViewById(R.id.checkBoxEstoyDeacuerdo);
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //Toast toast = Toast.makeText(this, "Estoy en onPause", Toast.LENGTH_LONG);
+        //toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 1000);
+        //toast.show();
 
         //PASOS PARA LA PERSISTENCIA DE DATOS
-        /**
-         * 1 - Crear un objeto de tipo SharedPreferences, Con esto se crea un objeto donde se guardara los datos de la pantalla
-         * El parametro: 'archivoConfiguracion' es el nombre de archivo donde se guardara las preferencias
-          */
-        datos = getSharedPreferences("archivoConfiguracion", MODE_PRIVATE);
+        /** 1 - Crear un objeto de tipo SharedPreferences, Con esto se crea un objeto donde se guardara los datos de la pantalla
+         * El parametro: 'archivoConfiguracion' es el nombre de archivo donde se guardara las preferencias   */
+        datos = getSharedPreferences("archivoPreferencias", MODE_PRIVATE);
 
         // 2 - Hacer esitable el objeto creado, con esto se hace que el objeto SharedPreferences sea editable
         SharedPreferences.Editor miEditor = datos.edit();
@@ -49,32 +55,34 @@ public class MainActivity extends AppCompatActivity {
         miEditor.putString("nombre", nombre.getText().toString());
         miEditor.putString("apellido", apellido.getText().toString());
 
+
         // 4 - Transferir la informacion a SharedPreferences, Con esto hacemos que las preferencias sean compartidas
         miEditor.apply();  // Esto se lleva la informacion al objeto
 
     }
 
-
-
-
     @Override
-    public void onStop() {
-        super.onStop();
-
-        Toast toast = Toast.makeText(this, "Estoy en onStop", Toast.LENGTH_LONG);
-        toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 1200);
-        toast.show();
+    public void onResume() {
+        super.onResume();
+        //Toast toast = Toast.makeText(this, "Estoy en onResume", Toast.LENGTH_LONG);
+        //toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 600);
+        //toast.show();
 
         // Con esto se crea un objeto donde se guardo los datos de la pantalla
-        datos = getSharedPreferences("archivoConfiguracion", MODE_PRIVATE);
+        datos = getSharedPreferences("archivoPreferencias", MODE_PRIVATE);
 
-        String datoApodo = datos.getString("apodo", "nada"); // dos parametros, 1: clave, 2: el dato a recibir
-        String datoEmail = datos.getString("email", "nada");
-        String datoNombre = datos.getString("nombre", "nada");
-        String datoApellido = datos.getString("apellido", "nada");
+        datoApodo = datos.getString("apodo", "nada");
+        apodo.setText(datoApodo);
+
+        datoEmail = datos.getString("email", "nada");
+        email.setText(datoEmail);
+
+        datoNombre = datos.getString("nombre", "nada");
+        nombre.setText(datoNombre);
+
+        datoApellido = datos.getString("apellido", "nada");
+        apellido.setText(datoApellido);
+
+
     }
-
-
-
-
 }
